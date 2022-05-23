e4bcf64ec1592fa2f167e69cfe2bca5ed73ebc2f
koanvl <koanvl@ukr.net>
2022-05-23 20:57:56 +0200

    showing the current weather

---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+let apiKey = "3c252ffd932e3d53580c8c66264e47b0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+function displayTemperature(response){
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+axios.get(apiUrl).then(displayTemperature);
