fe5e81a283e1b8de237af28be5e6041f8673e38f
koanvl <koanvl@ukr.net>
2022-05-27 12:08:11 +0200

    added final changes

--- src/main.js
+++ src/main.js
@@ -25,12 +25,13 @@ function formatDay(timestamp){
 
 function displayForecast(response){
   let forecast = response.data.daily;
+  
+  
   let forecastElement = document.querySelector("#forecast");
   forecastHTML = `<div class="row">`;
-  forecast.forEach(function(forecastDay, index){
+  forecast.forEach(function(forecastDay, index){     
     if (index < 6){
-      forecastHTML = forecastHTML +  `
-      
+      forecastHTML = forecastHTML +  `        
       <div class="col-2">
       <div class="forecast-day-wrap">
       <div class="forecast-day" class="mt-1">
@@ -40,87 +41,90 @@ function displayForecast(response){
       forecastDay.weather[0].icon
     }@2x.png" alt="icon">
     <div class="forecast-temperature">
-    <span class="max" class="forecast-day-temp-max">${Math.round(
-      forecastDay.temp.max
-      )}°</span> <span class="min" class="forecast-day-temp-max">${Math.round(
-        forecastDay.temp.min
-        )}°</span>
-        </div>
-        </div>
-        </div>
-        
-        `
-      }
-    });
-    forecastHTML = forecastHTML + `</div>`;
-    forecastElement.innerHTML = forecastHTML;
-  }
-  
-  function getForecast(coordinates){
-    let apiKey = "3c252ffd932e3d53580c8c66264e47b0";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayForecast);
-  }
-
-  function displayTemperature(response){
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windElement = document.querySelector("#wind");
-    let date = document.querySelector("#date");
-    let iconElement = document.querySelector("#icon");
-    celsiusTemperature = response.data.main.temp;
-    temperatureElement.innerHTML =`${Math.round(response.data.main.temp)}°`;
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    humidityElement.innerHTML = response.data.main.humidity;
-    windElement.innerHTML = Math.round(response.data.wind.speed);
-    date.innerHTML = formatDate(response.data.dt * 1000);
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    iconElement.setAttribute("alt", response.data.weather[0].description);
-   
-    getForecast(response.data.coord);
+    <span  class="forecast-day-temp-max">${
+      Math.round(forecastDay.temp.max)}°</span> <span class="forecast-day-temp-max">${Math.round(forecastDay.temp.min)}°</span>
+      </div>
+      </div>
+      </div>
+      
+      `      
+    }    
     
-  } 
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML; 
   
-  function search(city){
-    let apiKey = "3c252ffd932e3d53580c8c66264e47b0";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
-  }
+}
+
+function getForecast(coordinates){
+  let apiKey = "3c252ffd932e3d53580c8c66264e47b0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayTemperature(response){
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let date = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML =`${Math.round(response.data.main.temp)}°`;
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  date.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
   
-  function handleSearch(event){
-    event.preventDefault();
-    let searchInputElement = document.querySelector("#input-search");
-    search(searchInputElement.value);
-  }
+  getForecast(response.data.coord);
   
-  function displayFahrenheirTemperature(event){
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    let fahrenheitTemperature = celsiusTemperature*1.8 +32;
-    temperatureElement.innerHTML = `${Math.round(fahrenheitTemperature)}°`;
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-  }
-  function displayCelsiusTemperature(event){
-    event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = `${Math.round(celsiusTemperature)}°`;
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-  }
-  
-  
-  let celsiusTemperature = null;
-  let form = document.querySelector("#form-search");
-  form.addEventListener("submit", handleSearch);
-  
-  let fahrenheitLink = document.querySelector("#fahrenheit-link");
-  fahrenheitLink.addEventListener("click", displayFahrenheirTemperature);
-  
-  let celsiusLink = document.querySelector("#celsius-link");
-  celsiusLink.addEventListener("click", displayCelsiusTemperature);
-  
-  search("Gijon");
+} 
+
+function search(city){
+  let apiKey = "3c252ffd932e3d53580c8c66264e47b0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSearch(event){
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#input-search");
+  search(searchInputElement.value);
+}
+
+function displayFahrenheirTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = celsiusTemperature*1.8 +32;
+  temperatureElement.innerHTML = `${Math.round(fahrenheitTemperature)}°`;
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");   
+}
+function displayCelsiusTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${Math.round(celsiusTemperature)}°`;
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");  
+}
+
+
+
+
+let celsiusTemperature = null;
+
+
+let form = document.querySelector("#form-search");
+form.addEventListener("submit", handleSearch);
+
+//let fahrenheitLink = document.querySelector("#fahrenheit-link");
+//fahrenheitLink.addEventListener("click", displayFahrenheirTemperature);
+
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Gijon");
